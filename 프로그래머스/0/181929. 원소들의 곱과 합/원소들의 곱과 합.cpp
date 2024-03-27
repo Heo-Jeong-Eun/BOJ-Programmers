2a2ee0980f148a31e02d0510e0026dfb2580a80a
J <1001l1000l1110@gmail.com>
2024-03-27 15:31:08 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int sum = 0, mul = 1;
+    
+    for (int i = 0; i < num_list.size(); i++)
+    {
+        sum += num_list[i];
+        mul *= num_list[i];
+    }
+    
+    if ((sum * sum) > mul) return 1;
+    else return 0;
+}
